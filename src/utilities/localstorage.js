5c97385594eb5cc2ff89b8db656462963293912b
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-05 16:28:28 +0600

    Code for Video 41-6 added

---
+++ src/utilities/localstorage.js
@@ -0,0 +1,23 @@
+const getStoredCart = () => { 
+    const storedCart = localStorage.getItem('cart');
+    
+    if(storedCart){
+        return JSON.parse(storedCart);
+    } 
+        return [];
+    
+    }
+
+    const saveCartToLS = cart => {
+        const cartStringified = JSON.stringify(cart);
+        localStorage.setItem('cart', cartStringified);
+    }
+
+    const addToLS = (id) => {
+        const cart = getStoredCart();
+        cart.push(id);
+        // save to local storage
+        saveCartToLS(cart);
+    }
+
+    export {addToLS}
